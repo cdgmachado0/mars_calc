c7c3bbc1dd6a9221214296c9650a8735ed8a6434
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2023-07-07 22:03:42 +0200

    Done with calculator

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,18 @@
 use std::io;
 
 fn main() {
+    println!("Enter your weigth (kg): ");
     let mut input = String::new();
 
-    some_fn(&mut input);
-    io::stdin().read_line(&mut input);
-    let mut mars_weight = calculate_weigth_on_mars(100.0);
-    mars_weight = mars_weight * 1000.0;
-    println!("Weight on Mars: {}kg", mars_weight);
+    io::stdin().read_line(&mut input).unwrap();
 
+    let weigth: f32 = input.trim().parse().unwrap();
+    let mars_weight = calculate_weigth_on_mars(weigth);
+
+    println!("Weight on Mars: {}kg", mars_weight);
 }
 
 
 fn calculate_weigth_on_mars(weight: f32) -> f32 {
     (weight / 9.81) * 3.711
-}
-
-fn some_fn(s: &mut String) {
-    s.push_str("a");
 }
